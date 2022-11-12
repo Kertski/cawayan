7c5415964202dcc2bb3c26484b8f15a93a0a5347
mrsakay12 <67614439+mrsakay12@users.noreply.github.com>
2022-11-12 23:30:43 +0800

    admin

---
+++ src/admin.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect} from 'react';
+import AdminHeader from './Admin/AdminHeader'
+import Footer from './HomePages/Footer'
+
+
+function admin() {
+  return (
+
+    <div>
+        <AdminHeader/>
+       
+        <Footer/>
+    
+    </div>
+    )
+  }
+  
+  export default admin
