62ca4174ec4e077ada3327524417f89f4c93dc43
mrsakay12 <67614439+mrsakay12@users.noreply.github.com>
2022-11-16 23:16:31 +0800

    Admin portal as of Nov 16

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Productlist from './productlist';
 import OrderList from './OrderList';
 import Dashboard from './Dashboard';
 import Customer from './Customer';
-import ProductInfo from './ProductInfo';
 
 const App = () => {
 
@@ -60,11 +59,14 @@ const App = () => {
                 <Route path="/cart" element={<Cart cart={cart} setCart={setCart} handleChange={handleChange}/>}/>
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/login" element={<Login/>}/>
-                <Route path="/checkout" element={<CheckoutPage size={cart.length}/>}/>
+                <Route path="/checkout" element={<CheckoutPage cart={cart}/>}/>
                 <Route path="/admin" element={<Admin/>}/>
                 <Route path="/products" element={<Productlist/>}/>
                 
                 <Route path="/orderlist" element={<OrderList/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/customers" element={<Customer/>}/>
+
             </Routes>
         </Router>
     </>
