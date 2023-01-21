1c3c49dc14bcf861da9e25f0bb03ffaeb29618f1
Kertski <kertyasmoralda@gmail.com>
2023-01-22 01:11:53 +0800

    deployment

--- src/HomePages/BestSellerContainer.js
+++ src/HomePages/BestSellerContainer.js
@@ -6,7 +6,7 @@ function BestSellerContainer({category}) {
     
     const [products, setProducts] = useState([]);
     useEffect(() =>{
-        fetch('http://localhost:8000/products')
+        fetch('http://cawayan-api.onrender.com/products')
         .then(response => response.json())
         .then(data => {
             setProducts(data);
